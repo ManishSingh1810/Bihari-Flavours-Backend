f334f2bc83f704f6bff07b33f91dfc17a2c57c22
ManishSingh1810 <manishsinghh1810@gmail.com>
2026-01-10 00:30:31 +1000

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -5,11 +5,16 @@ const nodemailer = require("nodemailer");
  * Uses Gmail SMTP with App Password
  */
 const mailTransporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.MAIL_HOST,                 // smtp.titan.email
+  port: Number(process.env.MAIL_PORT || 587),  // 587 recommended
+  secure: String(process.env.MAIL_SECURE) === "true", // false for 587, true for 465
+  
   auth: {
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD
   }
+    
+  tls: { rejectUnauthorized: false } // helps avoid TLS issues on some hosts
 });
 
 // Verify connection once on server start
@@ -22,3 +27,4 @@ mailTransporter.verify((err, success) => {
 });
 
 module.exports = mailTransporter;
+
