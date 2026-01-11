a1b8cc9d72e6032bdc7dd7658278c1db2c64bcd3
ManishSingh1810 <manishsinghh1810@gmail.com>
2026-01-11 23:00:44 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-backend server .js
+
 require("dotenv").config();
 // server.js
 const express = require("express");
@@ -77,3 +77,4 @@ app.get("/", (req, res) => {
 // --------------------
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
+
