3e17000fb88efe4e5f258fbc57e07621ef647ba5
ManishSingh1810 <manishsinghh1810@gmail.com>
2026-01-13 11:30:19 +1000

    Update resend.js

--- utils/resend.js
+++ utils/resend.js
@@ -6,14 +6,23 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
   const from = process.env.MAIL_FROM || "onboarding@resend.dev";
 
-  const response = await resend.emails.send({
-    from,
-    to,
-    subject,
-    html,
-  });
+//   const response = await resend.emails.send({
+//     from,
+//     to,
+//     subject,
+//     html,
+//   });
 
-  return response;
-};
+//   return response;
+// };
 
-module.exports = { sendEmail };
+// module.exports = { sendEmail };
+
+try {
+  const resp = await resend.emails.send({ from, to, subject, html });
+  console.log("✅ Resend sent:", resp);
+  return resp;
+} catch (err) {
+  console.error("❌ Resend failed:", err);
+  throw err;
+}
